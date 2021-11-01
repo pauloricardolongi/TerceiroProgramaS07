9fd2e03ca18fbada0029b8ffe9c9b6d593322aed
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-01 15:52:50 -0300

    vetor String

---
+++ src/course/Programa.java
@@ -0,0 +1,17 @@
+package course;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		String s = "paulo ricardo longi longi";
+		
+		String[] vect = s.split(" ");
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);
+		System.out.println(vect[3]);
+		
+
+	}
+
+}

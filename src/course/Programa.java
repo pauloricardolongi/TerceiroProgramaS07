35a39ef67867569730ad7143c58a0c48906e4526
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-02 03:45:25 -0300

    função

--- src/course/Programa.java
+++ src/course/Programa.java
@@ -1,32 +1,40 @@
 package course;
 
+import java.util.Scanner;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		String original = "abcde FGHIJ  ABC abc  DEFG  ";
-		String s01 = original.toLowerCase(); //converter para minúsculas
-		String s02 = original.toUpperCase(); //letras maiúsculas
-		String s03 = original.trim();//elemina os espaços nos cantos da string
-		String s04= original.substring(2);//a partir da letra 2
-		String s05= original.substring(2,9);//a partir de quanto quero recortar e o limite
-		String s06= original.replace('a','x');//trocar sempre que achar o a minúsculo pelo x minúsculo
-		String s07= original.replace("abc","xy"); //sempre que achar abc trocar por xy
 		
-		int i = original.indexOf("bc");
-		int j = original.lastIndexOf("bc");
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Original:- " + original + "-");
-		System.out.println("toLowerCase:- " + s01 + "-");
-		System.out.println("toUpperCase:- " + s02 + "-");
-		System.out.println("trim:- " + s03 + "-");
-		System.out.println("substring(2):- " + s04 + "-");
-		System.out.println("substring(2,9):- " + s05 + "-");
-		System.out.println("replace('a''x'):- " + s06 + "-");
-		System.out.println("replace('abc''xy'):- " + s07 + "-");
-		System.out.println("IndexOf 'bc': " + i);
-		System.out.println("LastIndex 'bc': " + j);
+		System.out.println("Entre com três números: ");
+		int a= sc.nextInt();
+		int b= sc.nextInt();
+		int c= sc.nextInt();
 		
+		int maior = max(a, b, c);
+		
+		showResult(maior);
+		
+		sc.close();		
 
+	}
+	public static int max(int x, int y, int z) {
+		int aux;
+		if (x > y && x > z) {
+			aux = x;
+		}
+		else if (y > z) {
+			aux = y;
+		}
+		else {
+			aux = z;
+		}
+		return aux;
+	}
+	public static void showResult(int value) {
+		System.out.println("Maior é = " + value);
 	}
 
 }
